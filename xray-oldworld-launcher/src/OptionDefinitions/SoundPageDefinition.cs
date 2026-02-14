a99940d1cf02eeaf7eba9e83b67b45e8d5a1cb35
GoozE <cameron.mostert@gmail.com>
2026-02-15 00:58:32 +0200

    Update for Steam Audio integration

--- xray-oldworld-launcher/src/OptionDefinitions/SoundPageDefinition.cs
+++ xray-oldworld-launcher/src/OptionDefinitions/SoundPageDefinition.cs
@@ -18,8 +18,9 @@ public static class SoundPageDefinition
                         ConsoleCommand = "snd_volume_eff", DefaultValue = "1.0", Min = 0, Max = 1, Step = 0.1 },
                     new() { Id = "music_volume", Type = OptionType.Track, ValueType = OptionValueType.Float,
                         ConsoleCommand = "snd_volume_music", DefaultValue = "0.8", Min = 0, Max = 1, Step = 0.1 },
-                    new() { Id = "eax", Type = OptionType.Check, ValueType = OptionValueType.Bool,
-                        ConsoleCommand = "snd_efx", DefaultValue = "true" },
+                    new() { Id = "audio_effects_quality", Type = OptionType.List, ValueType = OptionValueType.String,
+                        ConsoleCommand = "snd_audio_effects_quality", DefaultValue = "st_opt_medium",
+                        Content = new() { "st_opt_low", "st_opt_medium", "st_opt_high" } },
                     new() { Id = "dynamic_music", Type = OptionType.Check, ValueType = OptionValueType.Bool,
                         ConsoleCommand = "g_dynamic_music", DefaultValue = "false" },
                 }
